966b07c8c59ef3c62e2068f2074c85a16ee600fc
harrygcoppock <harrygcoppock@gmail.com>
2022-03-03 17:59:29 +0000

    coughvid preparation complete

--- src/prep_data/coughvid/.ipynb_checkpoints/prep_coughvid-checkpoint.py
+++ src/prep_data/coughvid/.ipynb_checkpoints/prep_coughvid-checkpoint.py
@@ -24,92 +24,65 @@ class PrepCoughVid():
 
     RANDOM_SEED = 42
 
-    def __init__(self):
+    def __init__(self, modality='audio_three_cough_url'):
         self.output_base= f'./data/audio_16k'
-        self.input_base = f'./data/public_dataset'
+        self.input_base = f'.data/public_dataset'
+        self.create_folds()
         if not os.path.exists(self.input_base):
             self.download_data('https://zenodo.org/record/4498364/files/public_dataset.zip?download=1')
 
     def download_data(self, url):
-        if not os.path.exists('./data/'):
-            os.makedirs('./data/')
         wget.download(url, out='./data/')
         with zipfile.ZipFile('./data/public_dataset.zip', 'r') as zip_ref:
             zip_ref.extractall('./data/')
-        os.system(f'rm -rf ./data/public_dataset.zip')
+        os.system(f'rm -rf .data/public_dataset.zip')
 
     def main(self):
         if not os.path.exists(self.output_base):
             os.makedirs(self.output_base)
-        self.audio_files = [audio_file for audio_file in os.listdir(self.input_base) if audio_file.endswith(".webm") or audio_file.endswith(".ogg")]
+        print('creating json')
+        self.create_json()
         # now iterate through the files
-        print('length of audio file: ', len(self.audio_files))
+        audio_files = [audio_file for audio_file in os.listdir(self.input_base) if audio_file.endswith(".webm") or audio_file.endswith(".ogg")]
         self.error_list = []
         self.tot_removed = 0
-        Parallel(n_jobs=-1, verbose=10, prefer='threads')(delayed(self.process_file)(id) for id in self.audio_files)
+        Parallel(n_jobs=-1, verbose=10, prefer='threads')(delayed(self.process_file)(id) for id in audio_files)
         
         print(f'Average fraction removed: {np.mean(self.tot_removed)}')
         
-        print('creating folds')
-        self.create_folds()
-        print('creating json')
-        self.create_json()
-        with open(f'{self.output_base}/audio_16k/errorlist.txt', "w") as output:
+        with open(f'{self.output_base}/audio_16k/{split}/errorlist.txt', "w") as output:
             output.write(str(self.error_list))
 
     def process_file(self, filename):
 
         try:
-            signal, sr = librosa.load(os.path.join(self.input_base, filename), sr=16000)
-            
-        except:
-            print(f"{filename} not possible to load. Total so far: {len(self.error_list)}")
+            signal, sr = librosa.load(filename, sr=16000)
+        except RuntimeError:
+            print(f"{filename} not possible to load. Total so far: {len(error_list)}")
             self.error_list.append(filename)
             return 
-        clipped_signal, frac_removed = self.remove_silence(signal, filename)
+        clipped_signal, frac_removed = self.remove_silence(signal)
         self.tot_removed += frac_removed
-        if '.ogg' in filename:
-            filename = filename.replace('.ogg', '.wav')
-        elif '.webm' in filename:
-            filename = filename.replace('webm', '.wav')
-        else:
-            raise f'Unaccounted for file extentsion: {filename}'
-        sf.write(f"{self.output_base}/{filename}", clipped_signal, 16000)
+        sf.write(f"{self.output_base}/audio_16k/{re.match('([^/]+$)', filename).group(0)}", clipped_signal, 16000)
         return 
         
     
     def create_folds(self):
         kfold = KFold(n_splits=5, shuffle=True, random_state=self.RANDOM_SEED)
-        self.folds = [[self.audio_files[idx] for idx in test]
-                 for (train, test) in kfold.split(self.audio_files)]
+        self.folds = [[self.train[idx] for idx in test]
+                 for (train, test) in kfold.split(self.train)]
 
     def create_json(self):
-        if not os.path.exists('./data/datafiles/'):
-            os.makedirs('./data/datafiles/')
-        #for fold in [1]:# we do not have the compute for 5 fold cross validation in pretraining just finetuning
-        fold = 1
-        train_list = [instance for instance in self.audio_files if instance not in self.folds[fold-1]]
-        validation_list = [instance for instance in self.audio_files if instance in self.folds[fold-1]]
-        #remove any files which failed to load
-        train_list = [i for i in train_list if i not in self.error_list]
-        validation_list = [i for i in validation_list if i not in self.error_list]
-        #change format to wav
-        train_list = [self.format_to_wav(i) for i in train_list]
-        validation_list = [self.format_to_wav(i) for i in validation_list]
-        assert not any(x in validation_list for x in train_list), 'there is cross over between train and validation'
-        with open('./data/datafiles/ciab_train_data_'+ str(fold) +'.json', 'w') as f:
-            json.dump({'data': self.list_to_dict(train_list)}, f, indent=1)
-        with open('./data/datafiles/ciab_validation_data_'+ str(fold) +'.json', 'w') as f:
-            json.dump({'data': self.list_to_dict(validation_list)}, f, indent=1)
-    
-    def format_to_wav(self, filename):
-        if '.ogg' in filename:
-            return filename.replace('.ogg', '.wav')
-        elif '.webm' in filename:
-            return filename.replace('webm', '.wav')
-        else:
-            raise f'Unaccounted for file extentsion: {filename}'
-    def list_to_dict(self, data):
+        for fold in [1]:# we do not have the compute for 5 fold cross validation in pretraining just finetuning
+            train_list = [instance for instance in self.train if instance not in self.folds[fold-1]]
+            validation_list = [instance for instance in self.train if instance in self.folds[fold-1]]
+            assert not any(x in validation_list for x in train_list), 'there is cross over between train and validation'
+            with open('./data/datafiles/ciab_train_data_'+ str(fold) +'.json', 'w') as f:
+                json.dump({'data': self.list_to_dict(train_list, 'train')}, f, indent=1)
+            with open('./data/datafiles/ciab_validation_data_'+ str(fold) +'.json', 'w') as f:
+                json.dump({'data': self.list_to_dict(validation_list, 'train')}, f, indent=1)
+
+    def list_to_dict(self, data, split):
         '''
         THe ssast library requires a json file in the following format
          {
